1d43d0f72612813818131cd77e7994e3e0a5c973
Esteban Acosta <estebanacosta19@gmail.com>
2021-01-26 21:03:45 -0500

    added two auxilary methods in order to find runs in a player's hand

--- Rummy/Player.java
+++ Rummy/Player.java
@@ -119,6 +119,33 @@ public class Player
         }
     }
 
+    /****
+     * Returns that selected card from their hand
+     * @param whichCard
+     * @return
+     */
+    public Card getCardInPlayerCards(int whichCard)
+    {
+        return this.playerCards.get(whichCard - 1);
+    }
+
+    /***
+     * Determines if this player has a specific card in their hand
+     * @param thisCard
+     * @return
+     */
+    public boolean containsThisCard(Card thisCard)
+    {
+        for (Card c : playerCards)
+        {
+            if (c.getSuit() == thisCard.getSuit() && c.getRank() == thisCard.getRank())
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
     public boolean hasASet()
     {
         //find all possible sets
@@ -216,10 +243,7 @@ public class Player
         
         for(Card c : copyPlayerCards)
         {
-            if(c.)
-            {
-                
-            }
+            
         }
 
         return runs;
@@ -232,8 +256,7 @@ public class Player
     }
 
     /***
-     * Creates an array list of all the ranks the player has in their hand
-     * @return
+     * @return an array list of all the ranks the player has in their hand
      */
     public ArrayList<Rank> getAllRanksPlayerHas()
     {
@@ -250,6 +273,27 @@ public class Player
 
         return ranks;
     }
+    
+    /***
+     * @return an array list of all the suits the player has in their hand
+     */
+    public ArrayList<Suit> getAllSuitsPlayerHas()
+    {
+        
+        ArrayList<Suit> suits = new ArrayList<Suit>();
+
+        for (int i = 0; i < playerCards.size(); i++)
+        {
+            if (!suits.contains(playerCards.get(i).getSuit()))
+            {
+                suits.add(playerCards.get(i).getSuit());
+            }
+
+        }
+
+        return suits;
+    }
+
 
     /***
      * Calculates how many times a certain rank appears in the player's hand
@@ -290,34 +334,47 @@ public class Player
 
         return timesRankAppears;
     }
-
-    /****
-     * Returns that selected card from their hand
-     * @param whichCard
-     * @return
-     */
-    public Card getCardInPlayerCards(int whichCard)
-    {
-        return this.playerCards.get(whichCard - 1);
-    }
-
+    
     /***
-     * Determines if this player has a specific card in their hand
-     * @param thisCard
-     * @return
+     * Calculates how many times a certain suit appears in the player's hand
      */
-    public boolean containsThisCard(Card thisCard)
+    public Hashtable<Suit, Integer> howManyTimesThisSuitAppears()
     {
-        for (Card c : playerCards)
+        int count = 0;
+
+        // get an array list of all the ranks this player has in their hand
+        ArrayList<Suit> suits = getAllSuitsPlayerHas();
+
+        // create a hash table where the key is rank and the value is the number of times that rank appears in a player's hand
+        Hashtable<Suit, Integer> timesSuitAppears = new Hashtable<Suit, Integer>();
+
+        // loop through the ranks the player has
+        for (Suit suit : suits)
         {
-            if (c.getSuit() == thisCard.getSuit() && c.getRank() == thisCard.getRank())
+            // loop through the cards the player has
+            for (int i = 0; i < playerCards.size(); i++)
             {
-                return true;
+                // if the rank the outer loop is on is equal to this card's rank
+                if (playerCards.get(i).getSuit() == suit)
+                {
+                    // add one to count
+                    count++;
+                }
             }
+
+            // place the rank and the number of times this rank appears in the table
+            timesSuitAppears.put(suit, count);
+
+            // reset the counter when it's time for the outer loop to move on to the next rank
+            count = 0;
+
         }
 
-        return false;
+        return timesSuitAppears;
     }
+
+
+
 
     /****
      * Prints all the runs this player has to the screen
@@ -466,90 +523,6 @@ public class Player
             }
         }
         return count;
-    }
-
-    /***
-     * Checks to see if the user has two or more of the same card in their hand.
-     * for example, if the user wants to to be able to put down two cards of the same rank down in the middle
-     * This method checks to see if a certain rank appears twice. If a certain rank appears twice or whatever
-     * number of times the user wants then the method will return true and if no rank appears twice or whatever
-     * number of times the user specified then then the method will return false. This will help the game
-     * determine whether or not the user can put down two cards or more cards of the same rank down
-     * @param numTimesSameRankAppears
-     * @return
-     */
-    public boolean hasManyCardsOfSameRank(int howManyCardsToPutDown)
-    {
-        // take a card from the cards in the player's hand
-        for (int i = 0; i < playerCards.size(); i++)
-        {
-            // store that card's rank
-            int rank = playerCards.get(i).getValueOfCard();
-
-            // have a variable to check to see how many times that card appears in the player's hand
-            int numTimesRankAppears = 0;
-
-            // loop through the player's hand
-            for (int j = 0; j < playerCards.size(); j++)
-            {
-                // check if the randomly selected card's rank is equal to the rank of the card
-                if (rank == playerCards.get(j).getValueOfCard())
-                {
-                    // add one to count
-                    numTimesRankAppears++;
-                }
-            }
-
-            // if the card of that specific rank appears that same number of times or more
-            // then
-            if (numTimesRankAppears >= howManyCardsToPutDown)
-            {
-                // that must mean that at least one of the cards in the player's hand appears more than
-                // or equal that set number of times
-                return true;
-            }
-        }
-
-        return false;
-
-    }
-
-    /***
-     * Determines if the player can play on their turn. Checks if any of their cards is bigger than the middle card, if they have a two,
-     * or if they have multiple cards of the same rank
-     * @param middleCard
-     * @param howManyCardsOfSameRankToPutDown
-     * @return
-     */
-    public boolean canPlayACard(Card middleCard, int howManyCardsOfSameRankToPutDown)
-    {
-
-        int count = 1;
-        // loop through the player's hand
-        for (Card c : playerCards)
-        {
-            // if the player has a two in their hand
-            if (c.getRank() == Rank.TWO)
-            {
-
-                return true;
-
-            }
-            // or has at least one card that's higher in value than the top card
-            else if (c.getValueOfCard() > middleCard.getValueOfCard())
-            {
-                if (howManyTimesThisRankAppears(count) >= howManyCardsOfSameRankToPutDown)
-                {
-                    return true;
-                }
-
-            }
-
-            count++;
-
-        }
-
-        return false;
     }
 
 }
